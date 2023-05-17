3db0bd159c55d1ee0ee4dc6c66f34a265e85bbdb
Killian malon <oui>
2023-05-18 00:52:22 +0200

    Auth controller

---
+++ Interface/API/controllers/auth.js
@@ -0,0 +1,37 @@
+import AuthService from '../services/auth.js'
+const AuthServiceInstance = new AuthService()
+
+//For Register Page
+export const registerView = (req, res) => {
+    res.render("register", {
+    } );
+}
+
+// For View 
+export const loginView = (req, res) => {
+
+    res.render("login", {
+    } );
+}
+
+
+export async function signup (req, res) {
+    const fields = req.body
+    const jwt = await AuthServiceInstance.signup({ fields })
+    if (!jwt) {
+        
+        res.redirect('/auth/register')
+    }
+    res.redirect('/auth/login')
+}
+
+export const logout = async (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+}
+
+
+  
+
+
+

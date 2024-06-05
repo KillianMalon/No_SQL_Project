9b43049b10bd2fb135c5178b2fcb00ad36d8ef25
Nathan 5hiroe <77256143+5hiroe@users.noreply.github.com>
2024-06-05 15:50:59 +0200

    Update cypress.config.js

--- Interface/API/cypress.config.js
+++ Interface/API/cypress.config.js
@@ -1,11 +1,40 @@
-import { defineConfig } from "cypress";
+const webpack = require('webpack');
+const { startDevServer } = require('@cypress/webpack-dev-server');
+const path = require('path');
 
-export default defineConfig({
+module.exports = defineConfig({
   video: false,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
+      on('dev-server:start', (options) =>
+        startDevServer({
+          options,
+          webpackConfig: {
+            resolve: {
+              alias: {
+                // Make sure the path to your node_modules is correct
+                'node:crypto': require.resolve('crypto-browserify'),
+                'node:net': require.resolve('net-browserify'),
+              },
+              fallback: {
+                crypto: require.resolve('crypto-browserify'),
+                net: require.resolve('net-browserify'),
+                stream: require.resolve('stream-browserify'),
+                buffer: require.resolve('buffer'),
+              },
+            },
+            plugins: [
+              new webpack.ProvidePlugin({
+                process: 'process/browser',
+                Buffer: ['buffer', 'Buffer'],
+              }),
+            ],
+          },
+        })
+      );
+      // Other setupNodeEvents code...
+      return config;
     },
     baseUrl: "http://localhost:8080",
   },

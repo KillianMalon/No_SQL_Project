f75d70b089e679bf33530e468058df66127bb556
Quentin Bernigole <quentin.bernigole@hotmail.com>
2024-06-05 16:33:19 +0200

    Rajout de tests graphiques pour l'inscription d'un utilisateur

--- Interface/API/cypress/e2e/ui_create_account.cy.js
+++ Interface/API/cypress/e2e/ui_create_account.cy.js
@@ -2,8 +2,11 @@ describe('Vérification que les informations sont présentes pour la création d
 
   it('Les informations de connexions sont visibles par l\'utilisateur', () => {
     cy.visit('http://localhost:8080/auth/register');
+    cy.get('label[for="email-input"]').should('contain', 'Adresse email');
     cy.get('input[name="email"]').should('be.visible');
+    cy.get('label[for="password-input"]').should('contain', 'Mot de passe');
     cy.get('input[name="password"]').should('be.visible');
-    cy.get('button[type="submit"]').should('be.visible');
-    });
+    cy.get('button[type="submit"]').should('be.visible').and('contain', 'S\'inscrire');
+    cy.get('a[href="login"]').should('be.visible').and('contain', 'Connectez-vous');
+  });
 });

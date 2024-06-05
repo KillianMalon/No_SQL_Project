39467250965411d35eeabe18383cb101cb185c0f
Nathan 5hiroe <77256143+5hiroe@users.noreply.github.com>
2024-06-05 15:54:56 +0200

    Update cypress.config.js

--- Interface/API/cypress.config.js
+++ Interface/API/cypress.config.js
@@ -1,39 +1,10 @@
 import { defineConfig } from "cypress";
-import webpack from 'webpack';
-import { startDevServer } from '@cypress/webpack-dev-server';
-import path from 'path';
 
 export default defineConfig({
   video: false,
 
   e2e: {
     setupNodeEvents(on, config) {
-      on('dev-server:start', (options) =>
-        startDevServer({
-          options,
-          webpackConfig: {
-            resolve: {
-              alias: {
-                'node:crypto': 'crypto-browserify',
-                'node:net': 'net-browserify',
-              },
-              fallback: {
-                crypto: 'crypto-browserify',
-                net: 'net-browserify',
-                stream: 'stream-browserify',
-                buffer: 'buffer',
-              },
-            },
-            plugins: [
-              new webpack.ProvidePlugin({
-                process: 'process/browser',
-                Buffer: ['buffer', 'Buffer'],
-              }),
-            ],
-          },
-        })
-      );
-      return config;
     },
     baseUrl: "http://localhost:8080",
   },

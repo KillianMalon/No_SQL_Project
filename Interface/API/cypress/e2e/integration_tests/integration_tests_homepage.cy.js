b1acd3be430978dde5f33f3015f07f5f9d553def
KillianMalon <killian.malon@epsi.fr>
2024-06-06 10:47:55 +0200

    isolation tests intégration (bug à résoudre)

--- Interface/API/cypress/e2e/integration_tests/integration_tests_homepage.cy.js
+++ Interface/API/cypress/e2e/integration_tests/integration_tests_homepage.cy.js
@@ -1,30 +1,64 @@
-describe('Movie Search and Detail Flow', () => {
-    it('should search for movies and view details', () => {
-      // Visit the home page
-      cy.visit('/');
-  
-      // Enter a search term in the search form (title)
-      cy.get('input[placeholder="Rechercher par titre"]').type('Bovines');
-  
-      // Submit the search form
-      cy.get('button').contains('Rechercher').click();
-  
-      // Verify that the results page is displayed
-      cy.url().should('include', '/movies');
-      cy.get('.card').contains('Bovines').should('be.visible');
-  
-      // Click on the detail button of the movie
-      cy.get('.card').contains('Bovines').parent().find('a').click();
-  
-      // Verify that the detail page is displayed with specific details
-      cy.url().should('include', '/movies/details');
-      cy.get('.card-text').should('be.visible');
-      cy.get('.list-group-item')
-        .should('contain', 'Année de sortie')
-        .and('contain', 'Réalisateur')
-        .and('contain', 'Acteurs')
-        .and('contain', 'Genre')
-        .and('contain', 'Durée');
+// cypress/e2e/ui_tests/ui_homepage.cy.js
+
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { expect } from 'chai';
+import Movies from '../../../models/movies.js';
+import moviesService from '../../../services/movies.js';
+
+let mongoServer;
+
+describe('MoviesService', () => {
+    before(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        const uri = mongoServer.getUri();
+
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     });
+
+    after(async () => {
+        await mongoose.disconnect();
+        await mongoServer.stop();
+    });
+
+    beforeEach(async () => {
+        const movies = [
+            { title: 'Movie 1', realisator: 'Realisator 1' },
+            { title: 'Movie 2', realisator: 'Realisator 2' },
+            { title: 'Movie 3', realisator: 'Realisator 1' }
+        ];
+        await Movies.insertMany(movies);
+    });
+
+    afterEach(async () => {
+        await Movies.deleteMany();
+    });
+
+  it('should search for movies and view details', () => {
+    // Visit the home page
+    cy.visit('/');
+
+    // Enter a search term in the search form (title)
+    cy.get('input[placeholder="Rechercher par titre"]').type('Bovines');
+
+    // Submit the search form
+    cy.get('button').contains('Rechercher').click();
+
+    // Verify that the results page is displayed
+    cy.url().should('include', '/movies');
+    cy.get('.card').contains('Bovines').should('be.visible');
+
+    // Click on the detail button of the movie
+    cy.get('.card').contains('Bovines').parent().find('a').click();
+
+    // Verify that the detail page is displayed with specific details
+    cy.url().should('include', '/movies/details');
+    cy.get('.card-text').should('be.visible');
+    cy.get('.list-group-item')
+      .should('contain', 'Année de sortie')
+      .and('contain', 'Réalisateur')
+      .and('contain', 'Acteurs')
+      .and('contain', 'Genre')
+      .and('contain', 'Durée');
   });
-  
+});
